db230e1e90b1930ec7aaafb90ad278337ec8c0c8
abdulwariskhan <hellokhan.ch@gmail.com>
2023-07-24 09:23:31 +0200

    Initial Commit

---
+++ Calculator SwiftUI/Calculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_SwiftUIApp.swift
+//  Calculator SwiftUI
+//
+//  Created by Khan on 24.07.23.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
