a6e29d7e2f789a0a445e90e933613cc3d57d7d2a
abdulwariskhan <hellokhan.ch@gmail.com>
2023-07-24 17:49:22 +0200

    completed UI Design

--- Calculator SwiftUI/ContentView.swift
+++ Calculator SwiftUI/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttons=[
+    ["7","8","9","X"],
+    ["4","5","6","X"],
+    ["1","2","3","X"],
+    ["0",".",".","X"]]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Calculator App!")
+        
+        ZStack(alignment:.bottom){
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            
+            VStack(spacing:12){
+                
+                HStack{
+                    Spacer()
+                    Text("33").foregroundColor(Color.white).font(.system(size:64))
+                    
+                }.padding()
+                
+                ForEach(buttons, id:\.self){
+                    row in
+                    HStack(spacing:12){
+                        ForEach(row,id:\.self){
+                            button in
+                            Text(button)
+                                .font(.system(size:32))
+                                .frame(width:self.buttonWidth(), height: self.buttonWidth())
+                                .foregroundColor(Color.white)
+                                .background(.yellow)
+                                .cornerRadius(self.buttonWidth())
+                        }
+                    }
+                }
+                
+            }.padding(.bottom)
         }
-        .padding()
+      
+     
+        
+    }
+    func buttonWidth()-> CGFloat{
+        return (UIScreen.main.bounds.width-5*12)/4
     }
 }
 
